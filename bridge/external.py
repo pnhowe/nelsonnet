66dd9ba51bb325509fd93e55c62a2a4f14be79b4
Peter Howe <pnhowe@gmail.com>
2025-05-29 13:18:13 -0600

    added timeouts to the get requests, also better sleep logic so it dosent drift

--- bridge/external.py
+++ bridge/external.py
@@ -36,7 +36,7 @@ def main():
     while True:
       if counter % COAL_CREEK_INTERVAL == 0:
         try:
-          req = requests.get( 'https://api.water.noaa.gov/nwps/v1/gauges/COAU1/stageflow/observed' )
+          req = requests.get( 'https://api.water.noaa.gov/nwps/v1/gauges/COAU1/stageflow/observed', timeout=7 )
           data = req.json()
           metric_list = []
           for item in data[ 'data' ][-10:]:  # we we are geting 30 min at a time, the data is every 5 min, so 6 plus a few to overlap, just in case
@@ -48,7 +48,7 @@ def main():
 
       if counter % UPPER_DITCH == 0:
         try:
-          req = requests.get( 'http://192.168.16.51/raw' )
+          req = requests.get( 'http://192.168.16.51/raw', timeout=7 )
           level, gpm = req.text.split( '\t' )
           metric_list = []
           metric_list.append( ( 'data.upper_ditch.level', ( -1, float( level ) ) ) )
@@ -60,7 +60,7 @@ def main():
 
       if counter % UPPER_POND_PRESSURE == 0:
         try:
-          req = requests.get( 'http://192.168.16.52/raw' )
+          req = requests.get( 'http://192.168.16.52/raw', timeout=7 )
           pressure, threshold = req.text.split( '\t' )
           metric_list = []
           metric_list.append( ( 'data.upper_pond.pressure', ( -1, float( pressure ) ) ) )
@@ -71,7 +71,7 @@ def main():
           print( 'Exception getting Upper Pond Pressure Data', e, e.__class__ )
 
       counter += 1
-      time.sleep( 60 ) # Wait 1 min
+      time.sleep( 60 - ( time.time() % 60 ) ) # Wait 1 min, rounding out the remainder of this min
 
   except Exception as e:
     print( "Got Exception", e, e.__class__ )
